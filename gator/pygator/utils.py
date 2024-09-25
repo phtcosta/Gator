5d42b7373743398eb3f84f3ea6e1773380725d98
Pedro Costa <phtcosta@gmail.com>
2024-09-25 11:11:43 -0300

    including arg (additional classpath)

--- gator/pygator/utils.py
+++ gator/pygator/utils.py
@@ -16,41 +16,20 @@ def remove_temp_dirs():
     global temp_dirs
     for directory in temp_dirs:
         shutil.rmtree(directory, ignore_errors=True)
-
-
-# def extract_number(cur_line):
-#     firstCom = cur_line.find("'")
-#     secondCom = cur_line.find("'", firstCom + 1)
-#     levelStr = cur_line[firstCom + 1: secondCom]
-#     return int(levelStr)
+        
 
 def extract_number(cur_line):
-    """Extrai o primeiro número inteiro de uma string.
-
-    Args:
-        string: A string de entrada.
-
-    Returns:
-        O número inteiro extraído, ou None se nenhum número for encontrado.
-    """
-
-    # Padrão para encontrar um ou mais dígitos
-    padrao = r'\d+'
-    resultado = re.search(padrao, cur_line)
-
-    if resultado:
-        return int(resultado.group())
+    pattern = r'\d+'
+    result = re.search(pattern, cur_line)
+    if result:
+        return int(result.group())
     else:
         return -1
 
 
 def extract_target_api(yml_path):
-    print("extract_target_api = {}".format(yml_path))
     with open(yml_path, 'r') as fd:
         for line in fd.readlines():
-            print("line={}".format(line))
-            if 'targetSdkVersion' in line:
-                print("********************* targetSdkVersion = {}".format(extract_number(line)))
-                # return 29
+            if 'targetSdkVersion' in line:               
                 return extract_number(line)
     return -1
